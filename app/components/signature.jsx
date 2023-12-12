ce652311dbe8716a5e259bcde56be433e69d396a
AdrienW86 <adrien_weiss@outlook.fr>
2023-12-12 15:25:25 +0100

    test

---
+++ app/components/signature.jsx
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react';
+import SignatureCanvas from 'react-signature-canvas';
+
+const SignaturePad = () => {
+  const signatureRef = useRef();
+
+  return (
+    <div>
+      <SignatureCanvas
+        ref={signatureRef}
+        canvasProps={{ width: 500, height: 200, className: 'signature-canvas' }}
+      />
+      <button onClick={() => signatureRef.current.clear()}>Effacer la signature</button>
+    </div>
+  );
+};
+
+export default SignaturePad;
---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client"
+
+import Signature from './components/signature';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Signature Page</h1>
+      <Signature />
+      {/* Autres éléments de votre page */}
+    </div>
+  );
+}
